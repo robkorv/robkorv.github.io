cff601303645bb6259f1bea67ae23cdf45594c71
Robbert Korving <r.korving@gmail.com>
2014-09-13 17:32:06 +0200

    transition to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var spawn = require('child_process').spawn;
 
-gulp .task('default', function() {
-  // place code for your default task here
+gulp.task('build', function(cb){
+  spawn('jekyll', ['build'], {stdio: 'inherit'}).on('close', cb);
+});
+
+gulp.task('serve', function(cb){
+  return spawn('jekyll', ['serve'], {stdio: 'inherit'});
+});
+
+gulp.task('watch', function(cb){
+  spawn('jekyll', ['serve', '-w', '--config', '_config.yml,_config.dev.yml'],
+  {stdio: 'inherit'});
 });
