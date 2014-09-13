5067700d1533b208ef155db8d1dbf0ee3bce2111
Robbert Korving <r.korving@gmail.com>
2014-09-13 19:46:13 +0200

    livereload is back with gulp and browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,32 @@
 var gulp = require('gulp');
 var spawn = require('child_process').spawn;
+var browserSync = require('browser-sync');
+var runSequence = require('run-sequence');
 
-gulp.task('build', function(cb){
-  spawn('jekyll', ['build'], {stdio: 'inherit'}).on('close', cb);
+gulp.task('build', function(callback){
+  spawn('jekyll', ['build'], {stdio: 'inherit'}).on('close', callback);
 });
 
-gulp.task('serve', function(cb){
-  return spawn('jekyll', ['serve'], {stdio: 'inherit'});
+gulp.task('build-dev', function(callback){
+  return spawn('jekyll', ['build', '--config=_config.yml,_config.dev.yml', '-q'],
+    {stdio: 'inherit'}).on('close', callback);
 });
 
-gulp.task('watch', function(cb){
-  spawn('jekyll', ['serve', '-w', '--config', '_config.yml,_config.dev.yml'],
-  {stdio: 'inherit'});
+gulp.task('browserSync', function(){
+  browserSync({
+    online: false,
+    open: false,
+    notify: false,
+    server: {
+      baseDir: '_site',
+    }
+  });
+});
+
+gulp.task('watch', ['build-dev', 'browserSync'], function(){
+  gulp.watch(['**/*', '!_site/**/*'], function(){
+    runSequence(['build-dev'], function(){
+      browserSync.reload();
+    });
+  });
 });
