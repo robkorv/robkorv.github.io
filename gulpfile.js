4bc4c2292d599aa3b0e235c2cd2765ae1e042eda
Robbert Korving <r.korving@gmail.com>
2015-07-18 15:53:53 +0200

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -5,38 +5,48 @@ var runSequence = require('run-sequence');
 var optipng = require('gulp-optipng');
 
 gulp.task('build', function(callback){
-  runSequence('jekyll-build', 'optipng', callback);
+    'use strict';
+    runSequence('jekyll-build', 'optipng', callback);
 });
 
 gulp.task('jekyll-build', function(callback){
-  spawn('jekyll', ['build'], {stdio: 'inherit'}).on('close', callback);
+    'use strict';
+    spawn('jekyll', ['build'], {stdio: 'inherit'}).on('close', callback);
 });
 
 gulp.task('optipng', function () {
-  var options = ['-o7'];
-  return gulp.src('_site/img/*.png').pipe(optipng(options)).pipe(gulp.dest('_site/img/'));
+    'use strict';
+    var options = ['-o7'];
+    return gulp.src('_site/img/*.png').pipe(
+        optipng(options)
+    ).pipe(gulp.dest('_site/img/'));
 });
 
 gulp.task('jekyll-build-dev', function(callback){
-  return spawn('jekyll', ['build', '--config=_config.yml,_config.dev.yml', '-q'],
-    {stdio: 'inherit'}).on('close', callback);
+    'use strict';
+    return spawn(
+        'jekyll', ['build', '--config=_config.yml,_config.dev.yml', '-q'],
+        {stdio: 'inherit'}
+    ).on('close', callback);
 });
 
 gulp.task('browserSync', function(){
-  browserSync({
-    online: false,
-    open: false,
-    notify: false,
-    server: {
-      baseDir: '_site',
-    }
-  });
+    'use strict';
+    browserSync({
+        online: false,
+        open: false,
+        notify: false,
+        server: {
+            baseDir: '_site'
+        }
+    });
 });
 
 gulp.task('watch', ['jekyll-build-dev', 'browserSync'], function(){
-  gulp.watch(['**/*', '!_site/**/*'], function(){
-    runSequence(['jekyll-build-dev'], function(){
-      browserSync.reload();
+    'use strict';
+    gulp.watch(['**/*', '!_site/**/*'], function(){
+        runSequence(['jekyll-build-dev'], function(){
+            browserSync.reload();
+        });
     });
-  });
 });
